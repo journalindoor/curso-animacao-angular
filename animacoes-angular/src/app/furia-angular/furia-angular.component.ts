44925e8173ace2cf4e3d96ba2f5e490bd7114bca
RSMJr <journal.indoor@gmail.com>
2020-09-20 19:45:41 -0300

    [add] animadas

--- animacoes-angular/src/app/furia-angular/furia-angular.component.ts
+++ animacoes-angular/src/app/furia-angular/furia-angular.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
 import { AnimationController, Animation } from '@ionic/angular';
 
 @Component({
   selector: 'app-furia-angular',
   templateUrl: './furia-angular.component.html',
-  styleUrls: ['./furia-angular.component.scss']
+  styleUrls: ['./furia-angular.component.scss'],
 })
-export class FuriaAngularComponent implements OnInit {
+export class FuriaAngularComponent implements OnInit, AfterViewInit {
+  @ViewChild('furia', { read: ElementRef, static: true })
+  elementToAnimate: ElementRef;
 
-  constructor() { }
+  constructor(private animationCtrl: AnimationController) {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const animation: Animation = this.animationCtrl
+      .create()
+      .addElement(this.elementToAnimate.nativeElement)
+      .duration(2000)
+      .easing('ease-in')
+      .delay(2000)
+      .iterations(4)
+      .fromTo('opacity', '1', '0')
+      .fromTo('transform', 'scale(1)', 'scale(0)');
+
+    animation.play();
   }
 
+  ngOnInit(): void {}
 }
