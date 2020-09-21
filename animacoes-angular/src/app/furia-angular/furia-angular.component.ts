e5d0660cb7ba1dc336d2875cda6f3f87442abf95
RSMJr <journal.indoor@gmail.com>
2020-09-21 17:41:02 -0300

    [add] Animacao de partida na Furia da Noite

--- animacoes-angular/src/app/furia-angular/furia-angular.component.ts
+++ animacoes-angular/src/app/furia-angular/furia-angular.component.ts
@@ -13,23 +13,35 @@ import { AnimationController, Animation } from '@ionic/angular';
   styleUrls: ['./furia-angular.component.scss'],
 })
 export class FuriaAngularComponent implements OnInit, AfterViewInit {
-  @ViewChild('furia', { read: ElementRef, static: true })
-  elementToAnimate: ElementRef;
 
   constructor(private animationCtrl: AnimationController) {}
+  @ViewChild('banco', { read: ElementRef, static: true })
+  banco: ElementRef;
+  @ViewChild('tanque', { read: ElementRef, static: true })
+  tanque: ElementRef;
+  @ViewChild('cano', { read: ElementRef, static: true })
+  cano: ElementRef;
+  @ViewChild('motor', { read: ElementRef, static: true })
+  motor: ElementRef;
+  @ViewChild('farol', { read: ElementRef, static: true })
+  farol: ElementRef;
 
-  ngAfterViewInit(): void {
-    const animation: Animation = this.animationCtrl
+  ngAfterViewInit(): void {}
+
+  partida() {
+    const balancando: Animation = this.animationCtrl
       .create()
-      .addElement(this.elementToAnimate.nativeElement)
-      .duration(2000)
-      .easing('ease-in')
-      .delay(2000)
-      .iterations(4)
-      .fromTo('opacity', '1', '0')
-      .fromTo('transform', 'scale(1)', 'scale(0)');
+      .addElement(this.banco.nativeElement)
+      .addElement(this.tanque.nativeElement)
+      .addElement(this.cano.nativeElement)
+      .addElement(this.motor.nativeElement)
+      .addElement(this.farol.nativeElement)
+      .duration(200)
+      .iterations(Infinity)
+      .fromTo('transform', 'translateY(0)', 'translateY(-2px)');
 
-    animation.play();
+
+    balancando.play();
   }
 
   ngOnInit(): void {}
